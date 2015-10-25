fbee7757708d96e097795f5c619566087282a1a4
max <maxivanov95@mail.ru>
2015-10-25 22:27:03 +0200

    google map

--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -1,3 +1,15 @@
-angular.module('main').controller('mapController', function() {
+angular.module('main').controller('mapController', function(DatasetFactory, FilterFactory) {
+    var vm = this;
 
+
+    DatasetFactory.getUnitedDataset(function(data) {
+        vm.datasets = data;
+        dataLoaded();
+    });
+
+    function dataLoaded() {
+        FilterFactory.registerOnFilterChangedEvent(function(fields) {
+
+        });
+    }
 });
