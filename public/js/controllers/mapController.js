bcd77405c88159f89c02b09c4f64318df8bc84e1
Max <maxivanov95@ukr.net>
2015-11-10 00:27:20 +0200

    move map on filter changed

--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -1,7 +1,6 @@
 angular.module('main').controller('mapController', function(DatasetFactory, FilterFactory) {
     var vm = this;
-
-    initMap();
+    var districts, coords;
 
     DatasetFactory.getUnitedDataset(function(data) {
         vm.datasets = data;
@@ -9,81 +8,87 @@ angular.module('main').controller('mapController', function(DatasetFactory, Filt
     });
 
     function dataLoaded() {
-        FilterFactory.registerOnFilterChangedEvent(function(fields) {
-
+        FilterFactory.registerOnFilterChangedEvent(function(filtered) {
+            console.log(filtered);
+            if(filtered.fields) {
+                var fields = filtered.fields;
+                for(var i = 0; i < fields.length; i++) {
+                    if(fields[i].id == 'district' && fields[i].selectedVal != -1) {
+                        highlightArea();
+                    }
+                }
+            }
         });
     }
 
-});
+    $.getJSON("/json", function( data ) {
+        districts = data;
+        activate();
+    });
 
-    var districts, coords;
+    function activate() {
 
-     $.getJSON("/json", function( data ) { 
-                districts = data;
-            });        
-
-
-   function initMap() {
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: {lat: 46.748359, lng: 30.150735},
-                zoom: 7
-            });
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 46.748359, lng: 30.150735},
+            zoom: 7
+        });
 
 
-            for (var i = 0; i < districts.length; i++) {
-                var district = new google.maps.Polygon({
-                    paths: districts[i].coords,
-                    strokeColor: 'black',
-                    strokeOpacity: 0.8,
-                    strokeWeight: 1,
-                    fillColor: '#C3E1FF',
-                    fillOpacity: 0.35
-            });
-
-            district.setMap(map);
-
-            google.maps.event.addListener(district, 'mouseover', function() {
-                 this.setOptions({fillColor: 'green'});
-            });
-
-            google.maps.event.addListener(district, 'mouseout', function() {
-                 this.setOptions({fillColor: '#C3E1FF'});
-            });
-
-            }
-        }
-           
-        function highlightArea() {
-
-                moveMap(46.60680405,30.3363759975028);
-               
-                 for (var i = 0; i < districts.length; i++) {
-                    if(districts[i].district == 'Belyaevskiy')
-                        coords = districts[i].coords;
-                 }
-
-
-
+        for (var i = 0; i < districts.length; i++) {
             var district = new google.maps.Polygon({
-                paths: coords,
-                strokeColor: '#FF0000',
+                paths: districts[i].coords,
+                strokeColor: 'black',
                 strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: '#FF0000',
+                strokeWeight: 1,
+                fillColor: '#C3E1FF',
                 fillOpacity: 0.35
             });
 
             district.setMap(map);
 
-           google.maps.event.addListener(district, 'click', function() {
-                 this.setMap(null);
+            google.maps.event.addListener(district, 'mouseover', function() {
+                this.setOptions({fillColor: 'green'});
             });
+
+            google.maps.event.addListener(district, 'mouseout', function() {
+                this.setOptions({fillColor: '#C3E1FF'});
+            });
+
+        }
+    }
+
+    function highlightArea() {
+
+        moveMap(46.60680405,30.3363759975028);
+
+        for (var i = 0; i < districts.length; i++) {
+            if(districts[i].district == 'Belyaevskiy')
+                coords = districts[i].coords;
+        }
+
+
+
+        var district = new google.maps.Polygon({
+            paths: coords,
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35
+        });
+
+        district.setMap(map);
+
+        google.maps.event.addListener(district, 'click', function() {
+            this.setMap(null);
+        });
 
     }
 
     function moveMap(lat, lng) {
         var center = new google.maps.LatLng(lat, lng);
-         map.panTo(center);
-         map.setZoom(10);
+        map.panTo(center);
+        map.setZoom(10);
     }
+});
 
