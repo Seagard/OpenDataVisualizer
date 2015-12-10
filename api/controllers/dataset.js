e0c8d6cd8727fbb088bb9b04aac319a31858f46e
Max <maxivanov95@ukr.net>
2015-12-10 08:24:53 +0200

    dataset controller fix

--- api/controllers/dataset.js
+++ api/controllers/dataset.js
@@ -15,20 +15,30 @@ function getDatasets(req, res) {
 
 function getUnitedDatasets(req, res) {
   var datasets = [];
-  switch(parseInt(req.query.id)) {
-    case 1:
-      datasets = [];
-      datasets.push(JSON.parse(fs.readFileSync('data/revenue.json')));
-      datasets.push(JSON.parse(fs.readFileSync('data/expence.json')));
-      res.json(analyse.joinDatasets(datasets));
-          break;
-    case 2:
-      datasets = [];
-      datasets.push(JSON.parse(fs.readFileSync('data/dataset1.json')));
-      datasets.push(JSON.parse(fs.readFileSync('data/dataset0.json')));
-      datasets.push(JSON.parse(fs.readFileSync('data/dataset2.json')));
-      res.json(analyse.joinDatasets2(datasets));
-          break;
+  if(req.query.id) {
+    switch(parseInt(req.query.id)) {
+      case 1:
+        datasets = [];
+        datasets.push(JSON.parse(fs.readFileSync('data/revenue.json')));
+        datasets.push(JSON.parse(fs.readFileSync('data/expence.json')));
+        res.json(analyse.joinDatasets(datasets));
+        break;
+      case 2:
+        datasets = [];
+        datasets.push(JSON.parse(fs.readFileSync('data/dataset1.json')));
+        datasets.push(JSON.parse(fs.readFileSync('data/dataset0.json')));
+        datasets.push(JSON.parse(fs.readFileSync('data/dataset2.json')));
+        res.json(analyse.joinDatasets2(datasets));
+        break;
+    }
+  } else {
+    datasets = [];
+    datasets.push(JSON.parse(fs.readFileSync('data/revenue.json')));
+    datasets.push(JSON.parse(fs.readFileSync('data/expence.json')));
+    datasets.push(JSON.parse(fs.readFileSync('data/dataset1.json')));
+    datasets.push(JSON.parse(fs.readFileSync('data/dataset0.json')));
+    datasets.push(JSON.parse(fs.readFileSync('data/dataset2.json')));
+    res.json(analyse.joinDatasets2(datasets));
   }
 }
 
