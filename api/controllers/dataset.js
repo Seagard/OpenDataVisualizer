ad84f24a3d100f98eac875d4c48fa6aa0a7bee3d
Max <maxivanov95@ukr.net>
2015-12-10 07:01:47 +0200

    new datasets, id parameter

--- api/controllers/dataset.js
+++ api/controllers/dataset.js
@@ -7,17 +7,32 @@ function getDatasets(req, res) {
   var datasets = [];
   datasets.push(JSON.parse(fs.readFileSync('data/revenue.json')));
   datasets.push(JSON.parse(fs.readFileSync('data/expence.json')));
+  datasets.push(JSON.parse(fs.readFileSync('data/dataset0.json')));
+  datasets.push(JSON.parse(fs.readFileSync('data/dataset1.json')));
+  datasets.push(JSON.parse(fs.readFileSync('data/dataset2.json')));
   res.json(datasets);
 }
 
 function getUnitedDatasets(req, res) {
   var datasets = [];
-  datasets.push(JSON.parse(fs.readFileSync('data/revenue.json')));
-  datasets.push(JSON.parse(fs.readFileSync('data/expence.json')));
-  res.json(analyse.joinDatasets(datasets));
+  switch(parseInt(req.query.id)) {
+    case 1:
+      datasets = [];
+      datasets.push(JSON.parse(fs.readFileSync('data/revenue.json')));
+      datasets.push(JSON.parse(fs.readFileSync('data/expence.json')));
+      res.json(analyse.joinDatasets(datasets));
+          break;
+    case 2:
+      datasets = [];
+      datasets.push(JSON.parse(fs.readFileSync('data/dataset1.json')));
+      datasets.push(JSON.parse(fs.readFileSync('data/dataset0.json')));
+      datasets.push(JSON.parse(fs.readFileSync('data/dataset2.json')));
+      res.json(analyse.joinDatasets2(datasets));
+          break;
+  }
 }
 
 module.exports = {
   getDatasets: getDatasets,
   getUnitedDataset: getUnitedDatasets
-}
+};
