a8d9485e1085db4bd5366c2d119960dd75b425b0
max <maxivanov95@ukr.net>
2016-03-24 22:04:15 +0200

    test datasets

--- server.js
+++ server.js
@@ -34,39 +34,6 @@ app.get('/json1', function(req, res) {
     res.send(JSON.parse(data));
 });
 
-app.listen(port);
-console.log('Server is listening on port 3000');
-
-//test
-//var analyse = require('./analyse/analyse.js');
-//var datasetsUrls = [
-//    {
-//        host: 'data.ngorg.od.ua',
-//        path: '/api/action/datastore/search.json?resource_id=437b67c2-269f-41ee-973e-dd91a741c1fd'
-//    },
-//    {
-//        host: 'data.ngorg.od.ua',
-//        path: '/api/action/datastore/search.json?resource_id=8d316559-72a9-477d-bb06-197047607c1f'
-//    },
-//    {
-//        host: 'data.ngorg.od.ua',
-//        path: '/api/action/datastore/search.json?resource_id=5f25e8b1-6a8d-47ce-9fee-6d39a13d9db8'
-//    }
-//]
-//
-//datasetsUrls.forEach(function(url, i) {
-//    analyse.loadData(url.host, url.path)
-//        .then(function(data) {
-//            console.log('Got data:');
-//            console.log(data);
-//            var obj = JSON.parse(data).result;
-//            obj.type = 'expence';
-//            fs.writeFile('data/dataset' + i +'.json', JSON.stringify(obj));
-//        }, function(err) {
-//            console.log('Error loading datasets');
-//            console.log(err);
-//        })
-//        .catch(function(err) {
-//            console.log(err);
-//        })
-//});
+app.listen(port, function() {
+  console.log('Server is listening on port ' + port + '...');
+});
