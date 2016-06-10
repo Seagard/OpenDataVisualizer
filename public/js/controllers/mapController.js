74185bd233f9722c685ee765256f260e18778a84
seagard <igorpostrygan@gmail.com>
2016-06-11 02:39:41 +0300

    fixed statistics and district display in infowindow

--- public/js/controllers/mapController.js
+++ public/js/controllers/mapController.js
@@ -1,7 +1,7 @@
-angular.module('main').controller('MapController', function(DatasetFactory, FilterFactory, $http, $scope) {
+angular.module('main')
+    .controller('MapController', function(DatasetFactory, FilterFactory, $http, $scope) {
     var vm = this;
     var markers = [];
-    //var records = DatasetFactory.getExampleDatasets()[2].result.records;
     vm.datasets = [];
 
     vm.isDataLoaded = false;
@@ -21,29 +21,17 @@ angular.module('main').controller('MapController', function(DatasetFactory, Filt
     $scope.getDataset = function() {
         DatasetFactory.getDatasetById($scope.selectedItem.name)
             .then(function(dataset) {
-                console.log("go");
-                console.log(dataset);
                 return dataset.result.records;
             })
             .then(function(dataset) {
                 $scope.dataset = dataset;
-                //$scope.districts.forEach(function (district) {
-                //    district.memorials = [];
-                //
-                //$scope.dataset.forEach(function (data) {
                     $scope.districts.forEach(function (data) {
-                        //console.log(data);
                         data.memorials = [];
                         $scope.dataset.forEach(function (record) {
-                            console.log(record);
                             if (data.district == record.Rajon)
                                 data.memorials.push(record);
                         })
                     })
-                //});
-            })
-            .then(function() {
-                console.log($scope.districts);
             })
     }
 
@@ -54,64 +42,46 @@ angular.module('main').controller('MapController', function(DatasetFactory, Filt
 
     $scope.events = {
         mouseover: mouseOver,
-        mouseout: mouseOut
+        mouseout: mouseOut,
+        click: click
     }
 
-    function mouseOver() {
-        this.fill.color = "green"
+    $scope.polygonControl = function(polygon) {
+        return polygon;
     }
 
-    function mouseOut() {
-        this.fill.color = "#C3E1FF"
+    function click(polygon, e, model, args) {
+    }
+
+    function mouseOver(polygon, e, model, args) {
+
+    }
+
+    function mouseOut(polygon, e, model, args) {
+
     }
 
     $http.get("/json").then(function (data) {
         $scope.districts = data.data;
         $scope.initialize();
         return $scope.districts;
-    }).then(function(districts) {
-        console.log($scope.dataset);
-
-
-    }).then(function() {
-        //$scope.districts.forEach(function (district) {
-        //    district.memorials = [];
-        //
-        //    $scope.dataset.forEach(function (data) {
-        //        district.coords.forEach(function (coords) {
-        //            if (coords.longitude.toFixed(4) == data.longitude && coords.latitude == data.latitude) {
-        //                data.district = district.district;
-        //            }
-        //        });
-        //    });
-        //});
-        //$scope.districts.forEach(function (data) {
-        //    //console.log(data);
-        //    data.memorials = [];
-        //    $scope.dataset.forEach(function (record) {
-        //        console.log(record);
-        //        if (data.district == record.district)
-        //            data.memorials.push(record);
-        //    })
-        //})
     });
 
     $scope.initialize = function () {
-        var promise = new Promise(function(resolve, reject) {
-            resolve($scope.districts);
-        })
-
         $scope.map = {
             center: {
                 latitude: 46.748359,
                 longitude: 30.150735
             },
             zoom: 7,
-            markers: []
+            markers: [],
+            control: {}
         };
 
+        var pId = 0;
         $scope.districts.forEach(function (data) {
             $scope.polygons.push({
+                id: data.district,
                 path: data.coords,
                 stroke: {
                     color: 'black',
@@ -125,75 +95,71 @@ angular.module('main').controller('MapController', function(DatasetFactory, Filt
                     color: '#C3E1FF',
                     opacity: 0.35
                 },
-                events: $scope.events
+                events: $scope.events,
+                control: $scope.polygonControl
             });
         })
-
-
-    }
+    };
 
     $scope.highlight = function (map) {
+        $scope.clusterOpt = {
+            styles: [
+                {
+                    url: "images/alien.png",width:63,height:93
+                },
+            ],
+            maxZoom: 10,
+            minimumClusterSize: 10,
+            averageCenter: true
+        }
+        var i = 0;
         $scope.dataset.forEach(function (data) {
+            $scope.polygons.forEach(function(polygon) {
+                if (google.maps.geometry.poly.containsLocation(new google.maps.LatLng(data.lat, data.lon),
+                        polygon.control.getPlurals().get(polygon.id).gObject))
+                    data.area = polygon.id;
+            });
+            i = i+1;
             var marker = {
-                id: Date.now(),
-                coords: {
-                    latitude: data.lat,
-                    longitude: data.lon
-                },
-                events: {
-                    mouseover: onMouseOver,
-                    mouseout: onMouseOut
-                },
+                id: i,
+                latitude: data.lat,
+                longitude: data.lon,
                 data: data
             };
             map.markers.push(marker);
+
         });
-
-        var marker1 = {
-            id: Date.now(),
-            coords: {
-                latitude: 45.847,
-                longitude: 29.6111
-            }
-        };
-        map.markers.push(marker1);
-
-        console.log(map.markers);
+        $scope.map.markers = map.markers;
     };
 
+        $scope.onClick = function(marker, eventName, model) {
+            console.log("Clicked!");
+            model.show = !model.show;
+        };
+
     $scope.highlightRegions = function() {
+        $scope.polygons.forEach(function (polygon) {
+            polygon.markers = 0;
+            $scope.map.markers.forEach(function (marker) {
+                if (marker.data.area === polygon.id)
+                    polygon.markers++;
+            })
+        });
 
-        $scope.districts.forEach(function(area) {
-            var fillColor;
-            var length = area.memorials.length;
-
-            if (length == 0) {
+        $scope.polygons.forEach(function(polygon) {
+            if (polygon.markers == 0) {
                 fillColor = "#ffffff";
             }
-            else if (defineRange(length, 1, 10)) {
+            else if (defineRange(polygon.markers, 0, 10)) {
                 fillColor = "blue";
             }
             else  {
                 fillColor = "red";
             }
-
-            $scope.polygons.push({
-                path: area.coords,
-                stroke: {
-                    color: 'black',
-                    weight: 1
-                },
-                editable: true,
-                draggable: true,
-                geodesic: false,
-                visible: true,
-                fill: {
-                    color: fillColor,
-                    opacity: 0.35
-                },
-            });
+            var area = polygon.control.getPlurals().get(polygon.id).model;
+            area.fill.color = fillColor;
         })
-    }
+    };
 
     $scope.reset = function(map) {
         $scope.polygons = [];
@@ -203,27 +169,5 @@ angular.module('main').controller('MapController', function(DatasetFactory, Filt
     function defineRange(value, min, max) {
         return value > min && value <= max;
     }
-
-    $scope.choose = function() {
-        console.log($scope.selectedItem);
-
-        if($scope.selectedItem.name === "Пам'ятники 1 та 2 свiтових вiйн") {
-            $scope.showActions = true;
-        } else {
-            $scope.showActions = false;
-        }
-    }
-
-    function onMouseOver() {
-        console.log('b');
-        $scope.windowOptions.visible = !$scope.windowOptions.visible;
-    };
-
-    function onMouseOut() {
-        console.log('br');
-        $scope.windowOptions.visible = false;
-    };
-
-    $scope.title = "Window Title!";
 });
 
